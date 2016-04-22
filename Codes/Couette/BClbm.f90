0918264646c52921e8cba0ef13a4602abd5ea826
Behafarid <farhad@farhad.colorado.edu>
2016-04-22 12:41:15 -0600

    Piston: Cleaning BClbm and changin q calculation from iterative to simple qCalcFarhad for simple geometry of the piston

--- Codes/Couette/BClbm.f90
+++ Codes/Couette/BClbm.f90
@@ -142,190 +142,173 @@ SUBROUTINE BounceBack2New(m,i,j,k,im1,jm1,km1,fbb)	! implements the (moving) bou
 !--------------------------------------------------------------------------------------------------
 IMPLICIT NONE
 
-INTEGER(lng), INTENT(IN) :: m,i,j,k,im1,jm1,km1			! index variables
-REAL(dbl), INTENT(OUT) :: fbb									! bounced back distribution function
-INTEGER(lng) :: ip1,jp1,kp1,ip2,jp2,kp2					! index variables
-REAL(dbl) :: cosTheta, sinTheta								! COS(theta), SIN(theta)
-REAL(dbl) :: ub, vb, wb											! wall velocity (x-, y-, z- components)
-REAL(dbl) :: q														! local wall distance ratio [(distance from current node to wall)/(distance to next node in that direction)]
-REAL(dbl) :: rijk													! radius of current node
-REAL(dbl) :: x1,y1,z1,x2,y2,z2,xt,yt,zt,ht,rt,vt				! temporary coordinates to search for exact boundary coordinate (instead of ray tracing) 
-INTEGER(lng) :: it			! loop index variables
+INTEGER(lng), INTENT(IN) :: m,i,j,k,im1,jm1,km1	! index variables
+REAL(dbl), INTENT(OUT) :: fbb			! bounced back distribution function
+INTEGER(lng) :: ip1,jp1,kp1,ip2,jp2,kp2		! index variables
+REAL(dbl) :: cosTheta, sinTheta			! COS(theta), SIN(theta)
+REAL(dbl) :: ub, vb, wb				! wall velocity (x-, y-, z- components)
+REAL(dbl) :: q					! local wall distance ratio [(distance to wall)/(distance to next node in that direction)]
+REAL(dbl) :: rijk				! radius of current node
+REAL(dbl) :: x1,y1,z1,x2,y2,z2,xt,yt,zt,ht,rt,vt! temporary coordinates to search for exact boundary coordinate (instead of ray tracing) 
+INTEGER(lng) :: it				! loop index variables
 
-ip1 = i + ex(m)													! i location of 1st neighbor in the m direction
-jp1 = j + ey(m)													! j location of 1st neighbor in the m direction
-kp1 = k + ez(m)													! k location of 1st neighbor in the m direction
+ip1 = i + ex(m)					! i location of 1st neighbor in the m direction
+jp1 = j + ey(m)					! j location of 1st neighbor in the m direction
+kp1 = k + ez(m)					! k location of 1st neighbor in the m direction
 
-ip2 = i + 2_lng*ex(m)											! i location of 2nd neighbor in the m direction
-jp2 = j + 2_lng*ey(m)											! j location of 2nd neighbor in the m direction
-kp2 = k + 2_lng*ez(m)											! k location of 2nd neighbor in the m direction
+ip2 = i + 2_lng*ex(m)				! i location of 2nd neighbor in the m direction
+jp2 = j + 2_lng*ey(m)				! j location of 2nd neighbor in the m direction
+kp2 = k + 2_lng*ez(m)				! k location of 2nd neighbor in the m direction
 
 
-IF((node(ip1,jp1,kp1) .EQ. FLUID) .AND. (node(ip2,jp2,kp2) .EQ. FLUID)) THEN		! continue with 2nd order BB if the two positive neighbors are in the fluid (most cases)
-
-!*****************************************************************************
-	rijk = x(im1)								! height at current location
-		 ! Initial fluid node guess
-                 x1=x(i)
-                 y1=y(j)
-                 z1=z(k)
+IF ((node(ip1,jp1,kp1) .EQ. FLUID) .AND. (node(ip2,jp2,kp2) .EQ. FLUID)) THEN 	!2nd order BB if two positive neighbors are in fluid (most cases)
+   rijk = x(im1)								! height at current location
+   !----- Initial fluid node guess
+   x1=x(i)
+   y1=y(j)
+   z1=z(k)
                 
-		 ! Initial solid node guess
-                 x2=x(im1)
-                 y2=y(jm1)
-                 z2=z(km1)
+   !----- Initial solid node guess
+   x2=x(im1)
+   y2=y(jm1)
+   z2=z(km1)
                  
-	 IF (k.NE.km1) THEN
-                 DO it=1,qitermax
-		   ! guess of boundary location 
-                   xt=(x1+x2)/2.0_dbl
-                   yt=(y1+y2)/2.0_dbl
-                   zt=(z1+z2)/2.0_dbl
+   IF (k.NE.km1) THEN
+      DO it=1,qitermax
+         !----- guess of boundary location 
+         xt=(x1+x2)/2.0_dbl
+         yt=(y1+y2)/2.0_dbl
+         zt=(z1+z2)/2.0_dbl
 
-      		   rt = xt
-                   IF (rijk .GE. rOut(k)) THEN
-		   	ht = ((zt-z(k))*rOut(km1)+(z(km1)-zt)*rOut(k))/(z(km1)-z(k))
-		   ELSE
-		   	ht = ((zt-z(k))*rIn(km1)+(z(km1)-zt)*rIn(k))/(z(km1)-z(k))
-		   END IF
+         rt = xt
 
-                   IF(rt.GT.ht) then
-                     x2=xt
-                     y2=yt
-                     z2=zt
-                   ELSE
-                     x1=xt
-                     y1=yt
-                     z1=zt
-                   END IF
-				   
-                 END DO
-		 x1=x(i)
-                 y1=y(j)
-                 z1=z(k)
+         IF (rijk .GE. rOut(k)) THEN
+            ht = ((zt-z(k))*rOut(km1)+(z(km1)-zt)*rOut(k))/(z(km1)-z(k))
+         ELSE
+            ht = ((zt-z(k))*rIn(km1)+(z(km1)-zt)*rIn(k))/(z(km1)-z(k))
+         END IF
+
+         IF (rt.GT.ht) then
+            x2=xt
+            y2=yt
+            z2=zt
+         ELSE
+            x1=xt
+            y1=yt
+            z1=zt
+         END IF
+      END DO
+
+      x1=x(i)
+      y1=y(j)
+      z1=z(k)
                  
-                 x2=x(im1)
-                 y2=y(jm1)
-                 z2=z(km1)
+      x2=x(im1)
+      y2=y(jm1)
+      z2=z(km1)
  
-                 q=sqrt((xt-x1)**2+(yt-y1)**2+(zt-z1)**2)/sqrt((x2-x1)**2+(y2-y1)**2+(z2-z1)**2)
-		 !write(*,*) 'q',q,zt,z1,z2,0.5*(z1+z2),rt,ht
-	 ELSE
-		 DO it=1,qitermax
-		   ! guess of boundary location 
-                   xt=(x1+x2)/2.0_dbl
-                   yt=(y1+y2)/2.0_dbl
-                   zt=(z1+z2)/2.0_dbl
+      q=sqrt((xt-x1)**2+(yt-y1)**2+(zt-z1)**2)/sqrt((x2-x1)**2+(y2-y1)**2+(z2-z1)**2)
+   ELSE
+      DO it=1,qitermax
+         !----- guess of boundary location 
+         xt=(x1+x2)/2.0_dbl
+         yt=(y1+y2)/2.0_dbl
+         zt=(z1+z2)/2.0_dbl
 
-      		   rt = xt
-		   IF (rijk .GE. rOut(k)) THEN
-			   ht = (rOut(km1)+rOut(k))/2.0_dbl
-		   ELSE
-			   ht = (rIn(km1)+rIn(k))/2.0_dbl
-		   END IF
+         rt = xt
 
-                   IF(rt.GT.ht) then
-                     x2=xt
-                     y2=yt
-                     z2=zt
-                   ELSE
-                     x1=xt
-                     y1=yt
-                     z1=zt
-                   END IF
-                 END DO
+         IF (rijk .GE. rOut(k)) THEN
+            ht = (rOut(km1)+rOut(k))/2.0_dbl
+         ELSE
+            ht = (rIn(km1)+rIn(k))/2.0_dbl
+         END IF
 
-		 x1=x(i)
-                 y1=y(j)
-                 z1=z(k)
+         IF (rt.GT.ht) then
+            x2=xt
+            y2=yt
+            z2=zt
+         ELSE
+            x1=xt
+            y1=yt
+            z1=zt
+         END IF
+      END DO
+
+      x1=x(i)
+      y1=y(j)
+      z1=z(k)
                  
-                 x2=x(im1)
-                 y2=y(jm1)
-                 z2=z(km1)
+      x2=x(im1)
+      y2=y(jm1)
+      z2=z(km1)
  
-                 q=sqrt((xt-x1)**2+(yt-y1)**2+(zt-z1)**2)/sqrt((x2-x1)**2+(y2-y1)**2+(z2-z1)**2)
-		 !write(*,*) 'q',q,zt,z1,z2,0.5*(z1+z2),rt,ht
-	 ENDIF
-		 cosTheta=xt/rt
-		 sinTheta=yt/rt
-	 IF (k.NE.km1) THEN
-		   IF (rijk .GE. rOut(k)) THEN
-			 vt = ((zt-z(k))*velOut(km1)+(z(km1)-zt)*velOut(k))/(z(km1)-z(k))
-		   ELSE
-			 vt = ((zt-z(k))*velIn(km1)+(z(km1)-zt)*velIn(k))/(z(km1)-z(k))
-		   END IF
-	 ELSE
-		   IF (rijk .GE. rOut(k)) THEN
-			 vt = (velOut(k)+velOut(km1))*0.5_dbl
-		   ELSE
-			 vt = (velIn(k)+velIn(km1))*0.5_dbl
-		   END IF
-	 ENDIF
-		ub = velIn(km1)	!0.0_dbl!0.0!vel(km1)*cosTheta								! x-component of the velocity at i,j,k
-		vb = 0.0_dbl	!0.0!vel(km1)*sinTheta								! y-component of the velocity at i,j,k
-		wb = 0.0_dbl	 !vt!vel(km1)!0.0_dbl									! only z-component in this case)
-	! make sure 0<q<1
-        IF((q .LT. -0.00000001_dbl) .OR. (q .GT. 1.00000001_dbl)) THEN 
-          OPEN(1000,FILE="error.txt")
-          WRITE(1000,*) "q=",q
-          WRITE(1000,*) "m=",m
-          WRITE(1000,*) "i=",i,"j=",j,"k=",k
-          WRITE(1000,*) "im1=",im1,"jm1=",jm1,"km1=",km1
-          CLOSE(1000)
-          STOP
-        END IF	
-	!q = max(q, 0.25_dbl)
-!*****************************************************************************
-!*****************************************************************************
-!! Original method used by Gino
-!  rijk = SQRT(x(im1)*x(im1) + y(jm1)*y(jm1))				! radius at current location
-!
-!  cosTheta = x(im1)/rijk										! COS(theta)
-!  sinTheta = y(jm1)/rijk										! SIN(theta)
-!  !cosTheta = x(im1)/r(km1)									! COS(theta)
-!  !sinTheta = y(jm1)/r(km1)									! SIN(theta)
-!
-!  ub = vel(km1)*cosTheta										! x-component of the velocity at i,j,k
-!  vb = vel(km1)*sinTheta										! y-component of the velocity at i,j,k
-!  wb = 0.0_dbl														! no z-component in this case)
-!
-!  CALL qCalc(m,i,j,k,im1,jm1,km1,q)							! calculate q					
-!*****************************************************************************
-		 !write(*,*) 'q-Gino',q,i,j,k,im1,jm1,km1
+      q=sqrt((xt-x1)**2+(yt-y1)**2+(zt-z1)**2)/sqrt((x2-x1)**2+(y2-y1)**2+(z2-z1)**2)
+   ENDIF
 
 
-  ! bounced back distribution function with added momentum
-  IF((q .LT. 0.5_dbl) .AND. (q .GT. -0.00000001_dbl)) THEN
-    fbb = q*(1.0_dbl + 2.0_dbl*q)*fplus(bb(m),i,j,k) 															&
-        + (1.0_dbl - 4.0_dbl*q*q)*fplus(bb(m),ip1,jp1,kp1) 													& 
-        - q*(1.0_dbl - 2.0_dbl*q)*fplus(bb(m),ip2,jp2,kp2) 													&
-        !+ 6.0_dbl*wt(m)*rho(i,j,k)*(ub*ex(m) + vb*ey(m) + wb*ez(m)) ! use actual rho
-        + 6.0_dbl*wt(m)*1.0_dbl*(ub*ex(m) + vb*ey(m) + wb*ez(m)) ! use rho = 1.0
+
+   !cosTheta=xt/rt
+   !sinTheta=yt/rt
+   !IF (k.NE.km1) THEN
+   !   IF (rijk .GE. rOut(k)) THEN
+   !	 vt = ((zt-z(k))*velOut(km1)+(z(km1)-zt)*velOut(k))/(z(km1)-z(k))
+   !   ELSE
+   !      vt = ((zt-z(k))*velIn(km1)+(z(km1)-zt)*velIn(k))/(z(km1)-z(k))
+   !   END IF
+   !ELSE
+   !   IF (rijk .GE. rOut(k)) THEN
+   !	 vt = (velOut(k)+velOut(km1))*0.5_dbl
+   !   ELSE
+   !      vt = (velIn(k)+velIn(km1))*0.5_dbl
+   !   END IF
+   !ENDIF
+
+   CALL qCalcFarhad(i,q)
+   ub = velIn(km1)								! x-component of the velocity at i,j,k
+   vb = 0.0_dbl									! y-component of the velocity at i,j,k
+   wb = 0.0_dbl									! only z-component in this case)
+
+   !------ make sure 0<q<1
+   IF ((q .LT. -0.00000001_dbl) .OR. (q .GT. 1.00000001_dbl)) THEN 
+      OPEN(1000,FILE="error.txt")
+      WRITE(1000,*) "q=",q
+      WRITE(1000,*) "m=",m
+      WRITE(1000,*) "i=",i,"j=",j,"k=",k
+      WRITE(1000,*) "im1=",im1,"jm1=",jm1,"km1=",km1
+      CLOSE(1000)
+      STOP
+   END IF	
+
+
+   !----- bounced back distribution function with added momentum
+   IF ((q .LT. 0.5_dbl) .AND. (q .GT. -0.00000001_dbl)) THEN
+      fbb = q*(1.0_dbl + 2.0_dbl*q)*fplus(bb(m),i,j,k) 				&
+          + (1.0_dbl - 4.0_dbl*q*q)*fplus(bb(m),ip1,jp1,kp1)    	  	& 
+          - q*(1.0_dbl - 2.0_dbl*q)*fplus(bb(m),ip2,jp2,kp2) 			&
+         !+ 6.0_dbl*wt(m)*rho(i,j,k)*(ub*ex(m) + vb*ey(m) + wb*ez(m)) 		! use actual rho
+          + 6.0_dbl*wt(m)*1.0_dbl*(ub*ex(m) + vb*ey(m) + wb*ez(m)) 		! use rho = 1.0
 
 	fmovingsum = fmovingsum + (6.0_dbl*wt(m)*(ub*ex(m) + vb*ey(m) + wb*ez(m)))
 	fmovingrhosum = fmovingrhosum + (6.0_dbl*wt(m)*rho(i,j,k)*(ub*ex(m) + vb*ey(m) + wb*ez(m)))
-  ELSE IF((q .GE. 0.5_dbl) .AND. (q .LT. 1.00000001_dbl)) THEN
-    fbb = fplus(bb(m),i,j,k)/(q*(2.0_dbl*q + 1.0_dbl)) 														&
-        + ((2.0_dbl*q - 1.0_dbl)*fplus(m,i,j,k))/q																&
-        - ((2.0_dbl*q - 1.0_dbl)/(2.0_dbl*q + 1.0_dbl))*fplus(m,ip1,jp1,kp1)							&
+ 
+   ELSE IF((q .GE. 0.5_dbl) .AND. (q .LT. 1.00000001_dbl)) THEN
+      fbb = fplus(bb(m),i,j,k)/(q*(2.0_dbl*q + 1.0_dbl)) 			&
+        + ((2.0_dbl*q - 1.0_dbl)*fplus(m,i,j,k))/q	    		   	&
+        - ((2.0_dbl*q - 1.0_dbl)/(2.0_dbl*q + 1.0_dbl))*fplus(m,ip1,jp1,kp1)	&
         !+ (6.0_dbl*wt(m)*rho(i,j,k)*(ub*ex(m) + vb*ey(m) + wb*ez(m)))/(q*(2.0_dbl*q + 1.0_dbl))	! Use actual rho
         + (6.0_dbl*wt(m)*1.0_dbl*(ub*ex(m) + vb*ey(m) + wb*ez(m)))/(q*(2.0_dbl*q + 1.0_dbl))		! Use rho = 1.0
 
 	fmovingsum = fmovingsum + (6.0_dbl*wt(m)*(ub*ex(m) + vb*ey(m) + wb*ez(m)))/(q*(2.0_dbl*q + 1.0_dbl))
 	fmovingrhosum = fmovingrhosum + (6.0_dbl*wt(m)*rho(i,j,k)*(ub*ex(m) + vb*ey(m) + wb*ez(m)))/(q*(2.0_dbl*q + 1.0_dbl))
-
-  ELSE
-    OPEN(1000,FILE='error-'//sub//'.txt')
-    WRITE(1000,*) "Error in BounceBack2() in ICBC.f90 (line 137): q is not (0<=q<=1)...? Aborting."
-    WRITE(1000,*) "q=",q,"(i,j,k):",i,j,k
-    CLOSE(1000)
-    STOP
-  END IF
-
+   ELSE
+      OPEN(1000,FILE='error-'//sub//'.txt')
+      WRITE(1000,*) "Error in BounceBack2() in ICBC.f90 (line 137): q is not (0<=q<=1)...? Aborting."
+      WRITE(1000,*) "q=",q,"(i,j,k):",i,j,k
+      CLOSE(1000)
+      STOP
+   END IF
 ELSE
-
-  CALL BounceBackL(m,i,j,k,im1,jm1,km1,fbb)
-
+   CALL BounceBackL(m,i,j,k,im1,jm1,km1,fbb)
 END IF
 
 !------------------------------------------------
