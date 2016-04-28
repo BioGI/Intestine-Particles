b9550c6b1d8454308af4fb0ef57cca05ef1c1df8
Behafarid <farhad@farhad.colorado.edu>
2016-04-28 16:37:20 -0600

    Piston: Adding help for generalized BC definition and some other cosmetic changes in BCscalar routine

--- Codes/Couette/BCscalar.f90
+++ Codes/Couette/BCscalar.f90
@@ -16,17 +16,24 @@ SUBROUTINE BC_Scalar(m,i,j,k,im1,jm1,km1,phiBC)				! implements the scalar BCs
 IMPLICIT NONE
 
 INTEGER(lng), INTENT(IN) :: m,i,j,k,im1,jm1,km1				! index variables
-REAL(dbl), INTENT(OUT) :: phiBC     					! scalar contribution from the boundary condition
+REAL(dbl), INTENT(OUT)   :: phiBC     					! scalar contribution from the boundary condition
+
 INTEGER(lng) :: ip1,jp1,kp1 						! neighboring nodes (2 away from the wall)
-REAL(dbl) :: q, rhoB,phiB,feq_m						! distance ratio from the current node to the solid node
-REAL(dbl) :: rhoAstar,phiAstar, PkAstar,feq_Astar,feq_Bstar 		! values of density and at the boundary, and contribution of scalar from the boundary and solid nodes
-REAL(dbl) :: fPlusBstar, rhoBstar, phiBstar, PkBstar 			! Values interpolated to Bstar location
-REAL(dbl) :: phiijk_m							! contribution of scalar streamed in the mth direction to (ip1,jp1,kp1)
-REAL(dbl) :: cosTheta, sinTheta						! COS(theta), SIN(theta)
-REAL(dbl) :: ub, vb, wb, ubb,vbb,wbb							! wall velocity (x-, y-, z- components)
-REAL(dbl) :: rijk 							! radius of the solid node
-REAL(dbl) :: x1,y1,z1,x2,y2,z2,xt,yt,zt,ht,rt,vt
-INTEGER(lng) :: it
+REAL(dbl)    :: q
+REAL(dbl)    :: rhoAstar, phiAstar, feq_Astar,  PkAstar	 		! values of density and at the boundary, and contribution of scalar from the boundary and solid nodes
+REAL(dbl)    :: rhoBstar, phiBstar, fPlusBstar, PkBstar 		! Values interpolated to Bstar location
+REAL(dbl)    :: cosTheta, sinTheta					! COS(theta), SIN(theta)
+REAL(dbl)    :: ub, vb, wb						! wall velocity (x-, y-, z- components)
+REAL(dbl)    :: rijk 							! radius of the solid node
+
+!===========================================================================
+! HELP: How to set different boundary conditions
+! BC Scalar-zero:       coeffPhi=1      coeffGrad= 0    coeffConst= 0
+! BC Scalar-Non-zero:   coeffPhi=1      coeffGrad= 0    coeffConst= phi_BC
+! BC-Flux-zero:         coeffPhi=0      coeffGrad= 1    coeffConst= 0
+! BC-Flux-Non-zero:     coeffPhi=0      coeffGrad= 1    coeffConst= dphi/dn
+! BC-Permeability:      coeffPhi=Pw/Dm  coeffGrad=-1    coeffConst= 0
+!===========================================================================
 
 CALL qCalcFarhad(i,q)		
 
@@ -52,17 +59,19 @@ IF(node(ip1,jp1,kp1) .NE. FLUID) THEN
   kp1 = k
 END IF	
 
-IF (coeffGrad .EQ. 0) then				! Dirichlet BC
+IF (coeffGrad .EQ. 0) then						! Dirichlet BC
    phiWall= coeffConst/coeffPhi
-ELSE 			     				! Calculates phiWall for flux BC (eq. 28 in paper)				
-   phiWall= ((phiTemp(i,j,k)*(1.0+q)*(1.0+q)/(1.0+2.0*q)) - (phiTemp(ip1,j,k)*q*q/(1.0+2.0*q)) - (q*(1+q)/(1+2.0*q))* (coeffConst/coeffGrad)) &
-          / (1.0- (q*(1+q)/(1+2.0*q))*(coeffPhi/coeffGrad))
+ELSE 			     						! Calculates phiWall for flux BC (eq. 28 in paper)				
+   phiWall= ( (phiTemp(i,j,k)*(1.0+q)*(1.0+q)/(1.0+2.0*q)) - 	&
+	      (phiTemp(ip1,j,k)*q*q/(1.0+2.0*q)) -		&
+	      (q*(1+q)/(1+2.0*q))* (coeffConst/coeffGrad)    ) 	&
+            / (1.0- (q*(1+q)/(1+2.0*q))*(coeffPhi/coeffGrad))
 END IF
 
 !----- Computing values at A* & scalar streamed from A* (Chpter 3 paper)
-rhoAstar= (rho(i,j,k)- rho(ip1,jp1,kp1))*(1+q)+ rho(ip1,jp1,kp1)	! Extrapolate the density
-CALL Equilibrium_LOCAL(m,rhoAstar,ub,vb,wb,feq_Astar)    		! Equibrium distribution function in the mth direction
-phiAstar= phiWall							! Getting phi at the solid surface
+rhoAstar= (rho(i,j,k)- rho(ip1,jp1,kp1))*(1+q)+ rho(ip1,jp1,kp1)	! Extrapolate density
+CALL Equilibrium_LOCAL(m,rhoAstar,ub,vb,wb,feq_Astar)    		! Equibrium distribution function in mth direction
+phiAstar= phiWall							! phi at solid surface
 PkAstar= (feq_Astar/rhoAstar- wt(m)*Delta)*phiAstar			! Contribution from wall in mth direction (0 if phiWall=0)
 
 !------ Computing values at B* & scalar streamed from B* (Chpter 3 paper)
