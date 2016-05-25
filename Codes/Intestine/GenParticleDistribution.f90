bf63dfa150e546f21586a88bb64e1b31b5efd557
Behafarid <farhad@farhad.colorado.edu>
2016-05-25 09:56:08 -0600

    GenParticleDistribution: minor modifications and new plotting features

--- Codes/Intestine/GenParticleDistribution.f90
+++ Codes/Intestine/GenParticleDistribution.f90
@@ -2,94 +2,97 @@
 !===================================================================================================
 !This subroutine creates the particle distribution list 
 !===================================================================================================
-
 IMPLICIT NONE
 
-real   			::   dmin,dmax,dcen,dg,dgmin,dgmax
-real   			::   sigma,miu,pi
-real   			::   vd,vdmin,vdmax,vtot,deltd,ntot,Vp_tot
-real   			::   testnp,testv
-integer			::   nptot,ngrp, i,j,k
-real,    allocatable 	::  vfrac(:),nfrac(:),np(:)
+REAL*8			:: dmin,dmax,dcen,dg,dd,dgmin,dgmax
+REAL*8			:: sigma,miu,pi
+REAL*8			:: vd,vdmin,vdmax,vtot,deltd,ntot
+REAL*8			:: testnp,testv
+REAL*8			:: vf,V_c, C_tot_Over_Cs, C_s, nu_P, C_tot, Vp_tot
+INTEGER			:: nptot, ngrp, i,j
+real,    allocatable 	:: vfrac(:),nfrac(:),np(:)
 integer, allocatable 	:: intnp(:)
 
+V_c		= 9.424778e12 		! (\mu m) ^3
+C_tot_Over_Cs	= 0.100000000000	! dimensionless
+C_s		= 3.3e-19              	! mole / (\mu m ^3)
+nu_P		= 268.0e12 		! (\mu m ^3) / mole  		
+C_tot		= C_s * C_tot_Over_Cs   ! mole / (\mu m ^3)
+Vp_tot  	= C_tot * nu_P * V_c    ! (\mu m) ^3
+write(*,*) 'Vp_tot',Vp_tot
 
-!----- Read the input parameters -------------------------------------------------------------------
-!write(*,*) "please enter the total number of particles"				!nptot=	500
-!read(*,*) nptot
-!write(*,*) "please enter the total number of bins" 				!ngrp= 	20
-!read(*,*) ngrp 
-!write(*,*) "please enter the minimum diameter"					!dmin= 	3.0
-!read(*,*) dmin
-!write(*,*) "please enter the maximum diameter"					!dmax= 	92.1
-!read(*,*) dmax 
-!write(*,*) "please enter the diameter at the center of the distribution"	!dcen= 52.0
-!read(*,*) dcen
-!write(*,*) "please enter the standard deviation"   				!sigma= 17.32
-!read(*,*) sigma
-!write(*,*) "please enter the medium diameter"	                                !miu= 52.0
-!read(*,*) miu
-!---------------------------------------------------------------------------------------------------
-
-nptot	= 500
-Vp_tot	= 833.5e5 ! 1667.0e5 
-ngrp	= 20
+ngrp	= 19
 dmin	= 10.0
-dmax	= 200.0
-dcen	= 100.0
+dmax	= 190.0
 miu	= 100.0
-sigma	= 30.0
+sigma	= 20.0
 
 allocate(vfrac(ngrp),nfrac(ngrp),np(ngrp))
-allocate(intnp(ngrp))
 
 pi	= 4.0 * atan(1.0)
-deltd 	= (dmax-dmin)/(ngrp-1) 		! (dmax-dmin)/(ngrp-1)
+deltd 	= (dmax-dmin)/(ngrp-1) 		
 vtot	= 0.0
 ntot	= 0.0
 vfrac(:)= 0.0
 nfrac(:)= 0.0
 
-do k= 1,ngrp
-   dgmin    = dmin- deltd/2.0 + (k-1)*deltd
-   dgmax    = dmin+ deltd/2.0 + (k-1)*deltd
-   vdmin    = 1.0/sqrt(2.*pi*sigma**2) * exp(-(dgmin-miu)**2/(2.*sigma**2))                    
-   vdmax    = 1.0/sqrt(2.*pi*sigma**2) * exp(-(dgmax-miu)**2/(2.*sigma**2))                    
-   vfrac(k) = (vdmin+vdmax)*deltd/2.0
-   vtot	    = vtot + vfrac(k)
-!  nfrac(k) = vfrac(k) / (4.0/3.0*pi* ((dgmin+dgmax)/4.0)**3)
-!  ntot	    = ntot + nfrac(k)
+DO i= 1,ngrp
+   dg= dmin + (i-1)*deltd
+        	
+   !----- Integrating inside each bin (descritized to 100 sections)
+   DO j= 1, 100
+      dd = (dg-(deltd/2.0)) + ((j-1)+0.5)*(deltd/100)
+      vf = (1.0/sqrt(2.*pi*sigma**2)) * exp(-(dd-miu)**2/(2.*sigma**2))
+      vfrac(i) = vfrac(i) + (deltd/100) * vf
+   END DO
+
+   vtot= vtot + vfrac(i)
+END DO
+
+!----- Computing the number of particles in each bin -----------------------------------------------
+open(49,file='Plot_file.dat')
+do i= 1,ngrp
+   dg       = dmin + (i-1)*deltd        	
+   vfrac(i) = vfrac(i) * (Vp_tot/vtot)
+   nfrac(i) = vfrac(i) / ( (4.0*pi/3.0)* (dg/2.0)**3)
+   ntot     = ntot + nfrac(i)
+   write(49,*) dg,  (vfrac(i)/deltd), ((int(nfrac(i)+0.5)*(4.0/3.0*pi*(dg/2.0)**3))/deltd), int(nfrac(i)+0.5) 
 end do
 
-!vfrac = vfrac / vtot
-!nfrac = nfrac / ntot
-!np    = nfrac * nptot
 
-do k= 1,ngrp
-   dg    	= dmin + (k-1)*deltd        	
-   vfrac(k) 	= vfrac(k) * (Vp_tot/vtot)
-   nfrac(k) 	= vfrac(k) / (4.0/3.0*pi* ((dg)/2.0)**3)
-   ntot     	= ntot + nfrac(k)
-   write(*,*) 'k,vfrac,nfrac',k,vfrac(k),nfrac(k)
-end do
-
-
+!----- Creating the Par_Dist file with particle distribution data ----------------------------------
 open(50,file='Par_Dist.dat')
 do i= 1,ngrp
-   dg    	= dmin + (i-1)*deltd        	
-   write(50,*) dg, 1./sqrt(2.*pi*sigma**2)*exp(-(dg-miu)**2/(2.*sigma**2)),      &
-               vfrac(i), nfrac(i), int(nfrac(i)+0.5)  
+   dg  	= dmin + (i-1)*deltd        
+   write(50,*) dg, 1./sqrt(2.*pi*sigma**2)*exp(-(dg-miu)**2/(2.*sigma**2)), vfrac(i), nfrac(i), int(nfrac(i)+0.5)  
 end do
 
-!----- Testing total number of particles and total volume
+!----- Testing total number of particles and total volume ------------------------------------------
 testnp=0.0
 testv=0.0
 do i= 1,ngrp
    TestNp= TestNp + int(nfrac(i)+0.5) 
-   
    TestV = TestV  + vfrac(i)
 end do
+
+write(*,*) '----------------------------------------------------------------------------------------'
 write(*,*) testnp,testv
+
+!-----OutPut file ----------------------------------------------------------------------------------
+open(51,file='Plot_file_Vexpected.dat')
+write(51,2) C_tot_Over_Cs, C_tot_Over_Cs
+write(51,2) Vp_tot, Vp_tot
+write(51,2) TestNp,TestNp 
+write(51,2) dmin, dmin
+write(51,2) sigma, sigma
+DO i= 1, 2200
+   dg= dmin
+   dd = (dg-(deltd/2.0)) + ((i-1)+0.5) * ((dmax-dmin)/2000)
+   vf = (1.0/sqrt(2.*pi*sigma**2)) * exp(-(dd-miu)**2/(2.*sigma**2))
+   write(51,2) dd,  vf
+END DO
+2 format (F25.8,F25.8)
+!-------------
 
 stop
 
