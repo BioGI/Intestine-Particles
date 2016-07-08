031dea6f4fcd60f3cc8a0fba6a6167b69b92f9fb
Behafarid <farhad@farhad.colorado.edu>
2016-07-07 18:11:01 -0600

    Intestine: Restart Option: parallel communication for drug conservation

--- Codes/Intestine/IC.f90
+++ Codes/Intestine/IC.f90
@@ -12,10 +12,12 @@ SUBROUTINE ICs	! sets the initial conditions
 !--------------------------------------------------------------------------------------------------
 IMPLICIT NONE  
 
-INTEGER(lng) :: i, j, k, m					! index variables
+INTEGER(lng) :: i, j, k, m, mpierr					! index variables
 REAL(dbl)    :: feq
 INTEGER(lng) :: imintemp, imaxtemp				! index variables
 REAL(dbl)    :: alpha,xmin, xmax,xmid
+REAL(dbl)    :: Drug_Initial_l, Drug_Released_Total_l
+REAL(dbl)    :: Drug_Absorbed_restart_l, Drug_Remained_in_Domain_l 
 CHARACTER(7) :: iter_char                        		! iteration stored as a character
 
 IF (restart) THEN						! restart from  file 
@@ -41,12 +43,16 @@ IF (restart) THEN						! restart from  file
         END DO
       END DO
    END DO
-   READ(50,*) Drug_Initial
-   READ(50,*) Drug_Released_Total
+   READ(50,*) Drug_Initial_l
+   READ(50,*) Drug_Released_Total_l
    READ(50,*) Drug_Absorbed_restart
-   READ(50,*) Drug_Remained_in_Domain
+   READ(50,*) Drug_Remained_in_Domain_l
    delphi_particle = 0.0_dbl                               	! Initialize the scalar contirbution from particles to 0.0. Once the particle
    CLOSE(50)
+   CALL MPI_BARRIER(MPI_COMM_WORLD,mpierr)
+   CALL MPI_ALLREDUCE(Drug_Initial_l, 		Drug_Initial,		1, MPI_DOUBLE_PRECISION, MPI_SUM, MPI_COMM_WORLD, mpierr)
+   CALL MPI_ALLREDUCE(Drug_Released_Total_l, 	Drug_Released_Total,	1, MPI_DOUBLE_PRECISION, MPI_SUM, MPI_COMM_WORLD, mpierr)
+   CALL MPI_ALLREDUCE(Drug_Remained_in_Domain_l,Drug_Remained_in_Domain,1, MPI_DOUBLE_PRECISION, MPI_SUM, MPI_COMM_WORLD, mpierr)
 
    iter0 = iter0 + 1						! set the initial iteration to one after last iteration from the previous run
 
