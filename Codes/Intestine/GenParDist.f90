eb3ab86fd143d80cfe88024201b8a88d2538cf58
Behafarid <farhad.behafarid@gmail.com>
2017-01-20 16:30:42 -0700

    GenParDist: Adding modified normal distributionto particle distribution options

--- Codes/Intestine/GenParDist.f90
+++ Codes/Intestine/GenParDist.f90
@@ -2,9 +2,9 @@
 !This subroutine creates the particle distribution list 
 !===================================================================================================
 IMPLICIT NONE
-INTEGER			:: Counter,i,j
+INTEGER			:: Flag_Normal_Dist, Counter,i,j
 REAL*8      :: Dose, nu_m, M_d, dmin,dmax,sigma,miu,pi
-REAL*8      :: d_end,d_up,d1,d2,eps,V_integ,V_par_i,Vf,Vp_tot,Vp_tot_Achieved, Rlist(10000000)
+REAL*8      :: d_end,d_up,d1,d2,eps,V_integ,V_par_i,Vf,Vf2,Vp_tot,Vp_tot_Achieved, Rlist(10000000)
 
 Write(*,*) 'Please enter the dosage (\mu g):'
 read(*,*) Dose
@@ -16,6 +16,8 @@ Write(*,*) 'Please enter the minimum cut-off diameter, D_min (micron):'
 read(*,*) dmin
 Write(*,*) 'Please enter the standard deviation of the particle distribution, Sigma (micron):'
 read(*,*) sigma
+Write(*,*) 'Please enter 1 for Normal_Distribution and 0 for Modified_Normal_Distribution:'
+read(*,*) Flag_Normal_Dist
 
 pi              = 4.0 * atan(1.0)
 nu_m            = 268.0e6  	           ! (\mu m)^3 / (\mu mol)  		
@@ -37,14 +39,21 @@ DO WHILE (d1.GE.dmin)
    DO WHILE (V_par_i .GE. V_integ)
 !      eps = eps + 1e-11  
       d1 =  d2 - eps
-      V_integ = V_integ + (1.0/sqrt(2.*pi*sigma**2)) * exp(-(((d2+d1)/2.0)-miu)**2/(2.*sigma**2)) * (d2-d1) *Vp_tot
+      IF (Flag_Normal_Dist .EQ. 1) THEN
+         V_integ = V_integ + (1.0/sqrt(2.*pi*sigma**2)) * exp(-(((d2+d1)/2.0)-miu)**2/(2.*sigma**2)) * (d2-d1) *Vp_tot
+      ELSE IF (Flag_Normal_Dist .EQ. 0) THEN
+         V_integ = V_integ + ((1.0/sqrt(2.*pi*sigma**2)) * exp(-(((d2+d1)/2.0)-miu)**2/(2.*sigma**2))) *( 0.5*(1.0+tanh(0.2*((d2+d1)/2.0) -3.0))) *((d2-d1) *Vp_tot)
+      ELSE
+         write(*,*) 'wrong choice for choosing between Normal_Distribution and Modified_Normal_Distribution'
+      END IF    
       V_par_i = (4.0/3.0)* PI* ((d1+d2)/4.0)**3.0
       d2 = d1
    END DO
    Rlist(Counter)= (d1+d2)/2.0
    Vf = (V_par_i/Vp_tot)/(d_up-d1) 
+   Vf2= V_par_i/Vp_tot 
    Vp_tot_Achieved= Vp_tot_Achieved + V_Par_i
-   write(49,*) Counter, Rlist(Counter),  V_integ, V_par_i, Vf
+   write(49,*) Counter, Rlist(Counter),  V_integ, V_par_i, Vf,vf2
    d_up= d1
 END DO
 Close(49)
