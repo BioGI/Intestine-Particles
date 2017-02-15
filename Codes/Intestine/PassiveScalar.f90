cd29070fc97f21b1fae1eb0ea5d585d81550e684
Behafarid <farhad.behafarid@gmail.com>
2017-02-15 16:15:11 -0700

    PassiveScalar: Faster by removing unnecessary loops and MPI communications

--- Codes/Intestine/PassiveScalar.f90
+++ Codes/Intestine/PassiveScalar.f90
@@ -50,38 +50,23 @@ REAL(dbl)   :: zcf3                                         ! Cell volume
 
 CALL IC_Drug_Distribution 									! sets/maintains initial distributions of scalar [MODULE: IC.f90]
 
-IF (iter .EQ. iter_Start_phi) THEN  						 			! Calculate the intial amount of scalar
-   phiTotal = 0.0_dbl
-   DO k=1,nzSub
-      DO j=1,nySub
-         DO i=1,nxSub
-            IF (node(i,j,k) .EQ. FLUID) THEN
-               phiTotal = phiTotal + phi(i,j,k)
-            END IF
-         END DO
-      END DO
-   END DO
-END IF
-
-
 !----- store the previous scalar values
-phiTemp             = phi+ delphi_particle
-
+phiTemp             = phi + delphi_particle
 Negative_phi_Counter= 0
 Negative_phi_Worst  = 0.0_dbl
 Negative_phi_Total  = 0.0_dbl
-
 Over_Sat_Counter    = 0
-Largest_phi	    = 0.0_dbl
+Largest_phi	        = 0.0_dbl
 Over_Sat_Total      = 0.0_dbl
+phiTotal            = 0.0_dbl
 
 !----- Stream the scalar
 DO k=1,nzSub
    DO j=1,nySub
       DO i=1,nxSub
          IF (node(i,j,k) .EQ. FLUID) THEN
+            phiTotal = phiTotal + phi(i,j,k)
             phi(i,j,k) = Delta * phiTemp(i,j,k)  
-
 
             DO m=0,NumDistDirs
                !-----  neighboring node --------------------------------------------------------------
@@ -94,8 +79,6 @@ DO k=1,nzSub
                   CALL BC_Scalar(m,i,j,k,im1,jm1,km1,phiBC) 
                   phi(i,j,k) = phi(i,j,k) + phiBC     
                   CALL AbsorbedScalarS(i,j,k,m,im1,jm1,km1,phiBC) 								! measure the absorption rate
-               ELSE IF((node(im1,jm1,km1) .LE. -1) .AND. (node(im1,jm1,km1) .GE. -numVilli)) THEN				! villi
-          
                ELSE
                   OPEN(1000,FILE="error.txt")
                   WRITE(1000,'(A75)') "error in PassiveScalar.f90 at Line 89: node(im1,jm1,km1) is out of range"
@@ -110,18 +93,19 @@ DO k=1,nzSub
             END DO
 
 !---------- Monitoring the negative phi
-            zcf3 = zcf*zcf*zcf* 1000000.0_dbl  											! node volume in physical units (cm^3) so drung units are "mole
-            IF (phi(i,j,k) .LT. 0.0_dbl) THEN
-               Negative_phi_Counter = Negative_phi_Counter + 1
-               Negative_phi_Total   = Negative_phi_Total   + phi(i,j,k)  
-               IF (phi(i,j,k) .LT. Negative_phi_Worst) THEN
-                  Negative_phi_Worst = phi(i,j,k)
+            IF (Flag_Rectify_Neg_phi) THEN
+               zcf3 = zcf*zcf*zcf* 1000000.0_dbl  											! node volume in physical units (cm^3) so drung units are "mole
+               IF (phi(i,j,k) .LT. 0.0_dbl) THEN
+                  Negative_phi_Counter = Negative_phi_Counter + 1
+                  Negative_phi_Total   = Negative_phi_Total   + phi(i,j,k)  
+                  IF (phi(i,j,k) .LT. Negative_phi_Worst) THEN
+                     Negative_phi_Worst = phi(i,j,k)
+                  END IF
+                  IF ( Flag_Rectify_Neg_phi) THEN
+                     phi(i,j,k) = 0.0_dbl
+                  END IF                    
                END IF
-               IF ( Flag_Rectify_Neg_phi) THEN
-                  phi(i,j,k) = 0.0_dbl
-               END IF                    
-            END IF
-
+            ENDIF
 !---------- Monitoring over saturation
 !            IF (phi(i,j,k) .GT. Cs_mol) THEN
 !               Over_Sat_Counter= Over_Sat_Counter + 1
@@ -138,23 +122,23 @@ END DO
 
 
 !----- Printing out the outputs for Monitoring Negative-phi issue
-CALL MPI_BARRIER(MPI_COMM_WORLD,mpierr)
-CALL MPI_ALLREDUCE(Negative_phi_Counter, Negative_phi_Counter_Global, 1, MPI_INTEGER,          MPI_SUM, MPI_COMM_WORLD, mpierr)
-CALL MPI_ALLREDUCE(Negative_phi_Total,   Negative_phi_Total_Global,   1, MPI_DOUBLE_PRECISION, MPI_SUM, MPI_COMM_WORLD, mpierr)
-CALL MPI_ALLREDUCE(Negative_phi_Worst,   Negative_phi_Worst_Global,   1, MPI_DOUBLE_PRECISION, MPI_MIN, MPI_COMM_WORLD, mpierr)
-
-IF (myid .EQ. master) THEN
-   IF (Negative_phi_Counter_Global .GE. 1) THEN
-       write(2118,*) iter, Negative_phi_Counter_Global, Negative_phi_Total_Global/S_intrinsic, Negative_phi_Worst_Global/S_intrinsic, Negative_phi_Total_Global/(S_intrinsic*Negative_phi_Counter_Global)
-       CALL FLUSH(2118)
+IF (Flag_Rectify_Neg_phi) THEN
+   CALL MPI_BARRIER(MPI_COMM_WORLD,mpierr)
+   CALL MPI_ALLREDUCE(Negative_phi_Counter, Negative_phi_Counter_Global, 1, MPI_INTEGER,          MPI_SUM, MPI_COMM_WORLD, mpierr)
+   CALL MPI_ALLREDUCE(Negative_phi_Total,   Negative_phi_Total_Global,   1, MPI_DOUBLE_PRECISION, MPI_SUM, MPI_COMM_WORLD, mpierr)
+   CALL MPI_ALLREDUCE(Negative_phi_Worst,   Negative_phi_Worst_Global,   1, MPI_DOUBLE_PRECISION, MPI_MIN, MPI_COMM_WORLD, mpierr)
+   IF (myid .EQ. master) THEN
+      IF (Negative_phi_Counter_Global .GE. 1) THEN
+          write(2118,*) iter, Negative_phi_Counter_Global, Negative_phi_Total_Global/S_intrinsic, Negative_phi_Worst_Global/S_intrinsic, Negative_phi_Total_Global/(S_intrinsic*Negative_phi_Counter_Global)
+          CALL FLUSH(2118)
+      END IF
    END IF
-END IF
-
+ENDIF
 
 !----- Monitoring the Over Saturation problem
-CALL MPI_ALLREDUCE(Over_Sat_Counter, Over_Sat_Counter_Global, 1, MPI_INTEGER,          MPI_SUM, MPI_COMM_WORLD, mpierr)
-CALL MPI_ALLREDUCE(Over_Sat_Total,   Over_Sat_Total_Global,   1, MPI_DOUBLE_PRECISION, MPI_SUM, MPI_COMM_WORLD, mpierr)
-CALL MPI_ALLREDUCE(Largest_phi,      Largest_phi_Global,      1, MPI_DOUBLE_PRECISION, MPI_MAX, MPI_COMM_WORLD, mpierr)
+!CALL MPI_ALLREDUCE(Over_Sat_Counter, Over_Sat_Counter_Global, 1, MPI_INTEGER,          MPI_SUM, MPI_COMM_WORLD, mpierr)
+!CALL MPI_ALLREDUCE(Over_Sat_Total,   Over_Sat_Total_Global,   1, MPI_DOUBLE_PRECISION, MPI_SUM, MPI_COMM_WORLD, mpierr)
+!CALL MPI_ALLREDUCE(Largest_phi,      Largest_phi_Global,      1, MPI_DOUBLE_PRECISION, MPI_MAX, MPI_COMM_WORLD, mpierr)
 
 !IF (myid .EQ. master) THEN
 !   IF (Over_Sat_Counter_Global .GE. 1) THEN 
