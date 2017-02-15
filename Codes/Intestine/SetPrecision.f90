2d2f8be73e06b87a1e32066c08d9196bfeba295d
Behafarid <farhad.behafarid@gmail.com>
2017-02-15 16:03:05 -0700

    SetPrecision: Reducing unnecessary accuracy and range of INTEGER & REAL Parameters

--- Codes/Intestine/SetPrecision.f90
+++ Codes/Intestine/SetPrecision.f90
@@ -6,9 +6,10 @@ MODULE SetPrecision  ! Sets Precision of REAL and INTEGER Variables
 IMPLICIT NONE
         
 ! Define two parameters that set precision of floating point arithmetic and number of digits in integers respectively
-INTEGER, PARAMETER :: dbl = SELECTED_REAL_KIND(13,307)	! floating point precision (double)
-!INTEGER, PARAMETER :: dbl = SELECTED_REAL_KIND(6)			! floating point precision (single)
-INTEGER, PARAMETER :: lng = KIND(10000000)					! maximum integer value ("long")
+!INTEGER, PARAMETER :: dbl = SELECTED_REAL_KIND(13,307)	! floating point precision (double)
+INTEGER, PARAMETER :: dbl = SELECTED_REAL_KIND(8,16)    ! floating point precision (double) 
+!INTEGER, PARAMETER :: lng = KIND(10000000)					! maximum integer value ("long")
+INTEGER, PARAMETER :: lng = KIND(32)
 
 !================================================    
 END MODULE SetPrecision
